397ae8c304bbda2bc68c747c32740b804d6b2db1
code4shiv19 <shivshankargupta5207@gmail.com>
2026-06-07 23:42:36 +0530

    last commit

--- Monument & Architecture/asgi.py
+++ Monument & Architecture/asgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heritage_travel_design.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'monument_and_architecture_travel_guide.settings'
+)
 
 application = get_asgi_application()
