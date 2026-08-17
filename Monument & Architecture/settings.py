fbc98340857f0faf984fcbae08b7cfcc71a1c366
Shiv Shankar Gupta <shivshankargupta5207@gmail.com>
2026-08-17 20:31:52 +0000

    Changes in allowed Hosts

--- Monument & Architecture/settings.py
+++ Monument & Architecture/settings.py
@@ -13,7 +13,11 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'monument-architecture-travel-guide-4kwr.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
